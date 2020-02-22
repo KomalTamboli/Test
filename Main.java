20517494002123b635bc82307dea6e2edc139cf0
KomalTamboli <komaltambolisr@gmail.com>
2020-02-22 14:52:44 +0530

    tr

---
+++ Main.java
@@ -0,0 +1,38 @@
+
+
+import java.util.*;
+class Main{
+public static void addition(int a,int b){
+int Ans=a+b;
+System.out.println("Addition of "+a+" and "+b+" = "+Ans);
+}
+public static void subtraction(int a,int b){ 
+int Ans=a-b;
+System.out.println("subtration of "+a+" and "+b+" = "+Ans);
+}
+public static void multiplication(int a,int b){ 
+int Ans=a*b;
+System.out.println("Multiplication of "+a+" and "+b+" = "+Ans);
+}
+public static void division(int a,int b){ 
+int Ans=a/b;
+System.out.println("Dvision of "+a+" and "+b+" = "+Ans);
+}
+public static void Modulus(int a,int b){ 
+int Ans=a%b;
+System.out.println("Modulus of "+a+" and "+b+" = "+Ans);
+}
+
+public static void main(String args[]){
+Scanner  sc=new Scanner(System.in);
+System.out.println("Enter the first input= ");
+int a=sc.nextInt();
+
+System.out.println("Enter the second  input= ");
+int b=sc.nextInt();
+Main.addition(a,b);
+Main.subtraction(a,b);
+Main.multiplication(a,b);
+
+}}
+
